0b94613df772842f49caed9c2baa7615445cf56e
Husam256 <62894890+Husam256@users.noreply.github.com>
2021-03-01 12:55:02 +0000

    Added imports

---
+++ App.js
@@ -0,0 +1,14 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+//import Home from './Components/Home';
+//import Navigator from './routes/homeStack';
+//import { AppNavigator } from './routes/AppNavigator';
+
+//import About from './Components/About';
+//import review from './Components/review';
+//import { NavigationContainer } from '@react-navigation/native';
+import {Text} from 'react-native-paper';
+
+export default function App() {
+  return <Text> hello world </Text>;
+}
