956c4e24cf5252ac2c496f406f6005f6073512b1
Husam256 <62894890+Husam256@users.noreply.github.com>
2021-03-07 15:38:33 +0000

    Further improvments

--- Components/Home.js
+++ Components/Home.js
@@ -2,7 +2,13 @@ import React, {useState} from 'react';
 import {StyleSheet, Text, View, TextInput} from 'react-native';
 import {Button} from 'react-native-paper';
 
-export default function App() {}
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
---
+++ Components/Review.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, TextInput} from 'react-native';
+import {Button} from 'react-native-paper';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Reviews!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#DEB887',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
